998f679ec280b4befdff820b49c5032380bda20f
BarringtonT <BarringtonThom@gmail.com>
2016-03-11 21:34:28 -0500

    Tet.py commit

---
+++ PythonTestFiles/test2.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Filename: test2
+# @Author: Barrington W. Thompson
+# @Date:   2016-03-11 21:33:29
+# @Last Modified by:   BarringtonT
+# @Last Modified time: 2016-03-11 21:33:34
